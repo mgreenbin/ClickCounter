98142254d07a8a3261ec712891421639df446d0d
Mark Greenberg <magreenberg@gmail.com>
2019-06-08 17:41:48 -0500

    Initial Commit

---
+++ ClickCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClickCounter
+//
+//  Created by Mark Greenberg on 6/8/19.
+//  Copyright © 2019 MarkStar Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
